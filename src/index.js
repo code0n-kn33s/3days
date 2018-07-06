99a5f5dbad4cfee89205a5c4da938a4caaf9d6aa
Alex <nagana12@gmail.com>
2018-07-07 00:16:53 +0300

    Hello React

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function SayHello() {
+  let item = "React"
+  return (
+    <div><h1>Hello {item}</h1></div>
+  )
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+ReactDOM.render(<SayHello />, document.getElementById('root'));
 registerServiceWorker();
